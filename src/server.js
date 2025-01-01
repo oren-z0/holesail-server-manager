f83648a87ea21bf7f72787d444b832e73d30682a
Oren <orenz0@protonmail.com>
2025-01-01 17:17:39 +0200

    run holesail server

--- src/server.js
+++ src/server.js
@@ -3,19 +3,28 @@ const fastify = require('fastify')({ logger: true });
 const path = require('path');
 const fs = require('fs/promises');
 const nodeCrypto = require('node:crypto');
-
-// Register static file serving
-fastify.register(require('@fastify/static'), {
-  root: path.join(__dirname, 'static'),
-});
-
-// Serve the main page
-fastify.get('/', (_request, reply) => {
-  reply.sendFile('index.html');
-});
-
+const HolesailServer = require('holesail-server');
 
 const dataFile = process.env.HSM_DATA_FILE && path.join(__dirname, '..', process.env.HSM_DATA_FILE);
+const holesailTargetPort = process.env.HSM_TARGET_PORT && Number(process.env.HSM_TARGET_PORT);
+const holesailTargetAddress = process.env.HSM_TARGET_ADDRESS;
+
+let holesailServer;
+
+async function serveSeed(seed) {
+  if (holesailServer) {
+    fastify.log.info('Holesail server already running, destroying.');
+    holesailServer.destroy();
+  }
+  fastify.log.info(`Starting holesail server to target: ${holesailTargetAddress}:${holesailTargetPort}`);
+  holesailServer = new HolesailServer();
+  await new Promise((resolve) => holesailServer.serve({
+    port: holesailTargetPort,
+    address: holesailTargetAddress,
+    buffSeed: seed,
+    secure: false,
+  }, resolve));
+}
 
 async function ensureDataFile() {
   try {
@@ -36,6 +45,16 @@ async function ensureDataFile() {
   return true;
 }
 
+// Register static file serving
+fastify.register(require('@fastify/static'), {
+  root: path.join(__dirname, 'static'),
+});
+
+// Serve the main page
+fastify.get('/', (_request, reply) => {
+  reply.sendFile('index.html');
+});
+
 // Routes
 fastify.get('/api/settings', async (request, reply) => {
   try {
@@ -43,6 +62,7 @@ fastify.get('/api/settings', async (request, reply) => {
     return {
       ...settings,
       title: process.env.HSM_TITLE,
+      publicKey: holesailServer.getPublicKey(),
     };
   } catch (err) {
     fastify.log.error('/api/settings GET failed', err);
@@ -56,7 +76,9 @@ fastify.post('/api/settings', async (request, reply) => {
     if (typeof seed !== 'string' || seed.length !== 64 || !/^[0-9a-fA-F]+$/.test(seed)) {
       reply.code(400).send({ error: 'Seed must be a 64 character hex string' });
     }
-    await fs.writeFile(dataFile, JSON.stringify({ seed: seed.toLowerCase() }));
+    const lowercaseSeed = seed.toLowerCase();
+    await fs.writeFile(dataFile, JSON.stringify({ seed: lowercaseSeed }));
+    await serveSeed(lowercaseSeed);
     return { success: true };
   } catch (err) {
     fastify.log.error('/api/settings POST failed', err);
@@ -67,9 +89,14 @@ fastify.post('/api/settings', async (request, reply) => {
 // Start the server
 async function start() {
   try {
+    if (!holesailTargetPort || !holesailTargetAddress ) {
+      throw new Error('HSM_TARGET_PORT and HSM_TARGET_ADDRESS environment variables are required');
+    }
     if (!await ensureDataFile()) {
       throw new Error('Failed to initialize data file');
     }
+    const settings = JSON.parse(await fs.readFile(dataFile, 'utf-8'));
+    await serveSeed(settings.seed);
     const port = Number(process.env.HSM_PORT) || 3000;
     await fastify.listen({ port });
     fastify.log.info(`Server running on port ${port}`);
@@ -79,4 +106,6 @@ async function start() {
   }
 }
 
-void start();
+if (require.main === module) {
+  void start();
+}
